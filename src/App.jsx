b092728b95080912cd7da8cb548ed1b10ea0d6f2
Esben Inglev <esbeninglev@gmail.com>
2024-01-07 17:34:45 +0100

    basePath variable sat ind

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,20 @@ import Foto from "./Foto"
 import Om from "./Om"
 import Kontakt from "./Kontakt"
 
+const basePath = import.meta.env.MODE === 'production' ? '/EjgilWestergaard/' : '/';
+
 function App() {
-  return <Router>
+  return <Router basename={basePath}>
     <Header />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/skulpturer" element={<Skulpturer />} />
-      <Route path="/video" element={<Video />} />
-      <Route path="/cv" element={<CV />} />
-      <Route path="/avisudklip" element={<Avisudklip />} />
-      <Route path="/foto" element={<Foto />} />
-      <Route path="/om" element={<Om />} />
-      <Route path="/kontakt" element={<Kontakt />} />
+      <Route path={""} element={<Home />} />
+      <Route path={"skulpturer"} element={<Skulpturer />} />
+      <Route path={"video"} element={<Video />} />
+      <Route path={"cv"} element={<CV />} />
+      <Route path={"avisudklip"} element={<Avisudklip />} />
+      <Route path={"foto"} element={<Foto />} />
+      <Route path={"om"} element={<Om />} />
+      <Route path={"kontakt"} element={<Kontakt />} />
     </Routes>
   </Router>
 }
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useRef } from 'react';
 import Viewer from 'viewerjs';
 import 'viewerjs/dist/viewer.css';
 
+const basePath = import.meta.env.MODE === 'production' ? '/EjgilWestergaard' : '';
+
 const ImageGallery = () => {
-  const images = {
+  const rawImages = {
     '/img/Skulpturer/thumbnails/001BølgenThumbnail.jpg' : ['img/Skulpturer/001Bølgen.jpg', 'img/Skulpturer/001BølgenA.jpg', 'img/Skulpturer/001BølgenB.jpg', 'img/Skulpturer/001BølgenC.jpg', 'img/Skulpturer/001BølgenD.jpg'],
     '/img/Skulpturer/thumbnails/002TukakMaskespilThumbnail.jpg' : ['img/Skulpturer/002Tukak Maskespil.jpg', 'img/Skulpturer/002Tukak MaskespilA.jpg', 'img/Skulpturer/002Tukak MaskespilB.jpg', 'img/Skulpturer/002Tukak MaskespilC.jpg', 'img/Skulpturer/002Tukak MaskespilD.jpg'],
     '/img/Skulpturer/thumbnails/003BorgermusikkenThumbnail.jpg' : ['img/Skulpturer/003Borgermusikken.jpg', 'img/Skulpturer/003BorgermusikkenA.jpg', 'img/Skulpturer/003BorgermusikkenB.jpg'],
@@ -11,6 +13,11 @@ const ImageGallery = () => {
     '/img/Skulpturer/thumbnails/005SommerfuglSkitseThumbnail.jpg' : ['img/Skulpturer/005Sommerfugl.jpg', 'img/Skulpturer/005SommerfuglA.jpg', 'img/Skulpturer/005SommerfuglSkitse.jpg'],
     '/img/Skulpturer/thumbnails/005SommerfuglThumbnail.jpg' : ['img/Skulpturer/005Sommerfugl.jpg', 'img/Skulpturer/005SommerfuglA.jpg', 'img/Skulpturer/005SommerfuglSkitse.jpg'],
   };
+
+  const images = Object.entries(rawImages).reduce((acc, [thumbnail, imageSet]) => {
+    acc[basePath + thumbnail] = imageSet.map(img => basePath + img);
+    return acc;
+  }, {});
 
   const handleClick = (imageSet) => {
     const viewerImages = document.createElement('div');
@@ -33,7 +40,7 @@ const ImageGallery = () => {
   return (
     <div className="image-container">
       {Object.entries(images).map(([thumbnail, imageSet], index) => (
-          <img src={thumbnail} className="thumbnail" onClick={() => handleClick(imageSet)} alt={`Thumbnail ${index}`} />
+        <img src={thumbnail} className="thumbnail" onClick={() => handleClick(imageSet)} alt={`Thumbnail ${index}`} />
       ))}
     </div>
   );
